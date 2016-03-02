723ecfba5b56de9c2695ff256bee86a7011cb365
Victor Nikitash <nikitashvictor@pdffiller.com>
2016-03-02 10:50:33 +0200

    test

--- tests/IPValidatorTest.php
+++ tests/IPValidatorTest.php
@@ -1,6 +1,6 @@
 <?php
 
-require "vendor/autoload.php";
+require __DIR__."../../vendor/autoload.php";
 use Validator\IPValidator as IPValidator;
 
 class IPValidatorTest extends PHPUnit_Framework_TestCase {
